dd577a013861b9757f43c398c811d84091e027e1
Greg Linscheid <linscheid.greg@gmail.com>
2019-10-24 21:49:50 -0700

    Removed unecessary variable

--- run_container.sh
+++ run_container.sh
@@ -3,8 +3,6 @@ if [[ "$(docker images -q greglinscheid/polynote:latest 2> /dev/null)" == "" ]];
   source build_image.sh
 fi
 
-VOLUME_NAME='poly-notes'
-
 cmd='docker run -p 8193:8193 -v poly-notes:/usr/src/app/polynote --name=polynote -d -t greglinscheid/polynote:latest'
 echo $cmd
 $cmd
